d8b69d0660fed4f3f8a025bf4bc1d74a922c3ce5
ericpereira25 <ericpereira25@gmail.com>
2016-06-13 22:16:10 +0530

    Added Comments in R scripts

--- Plot1.R
+++ Plot1.R
@@ -1,3 +1,14 @@
+
+############################################################################################################
+#'Project 1 - Exploratory Data Analysis - Week 1
+#'
+#'Plot1: Global Active Power vs. Frecuency
+#'
+#'Author: Eric Pereira 
+#'
+#'Dated:13-06-2016
+#############################################################################################################
+
 myDF1<-read.table(file=file.choose(),sep=";",stringsAsFactors = FALSE,na.strings = "?",blank.lines.skip = TRUE,skip = 66638,nrow=2879,col.names = c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3"))
 
 
